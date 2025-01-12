42541e4e186947c32eb67dd0d0d55a62465d6089
42kharuya <haruya.0411.k@gmail.com>
2025-01-13 05:15:26 +0900

    to fix on linux

---
+++ get_next_line.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kharuya <kharuya@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/16 23:01:35 by kharuya           #+#    #+#             */
+/*   Updated: 2024/12/31 16:41:55 by kharuya          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+// include
+# include <stdlib.h>
+# include <unistd.h>
+
+// buffer
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 3
+# endif
+
+// prototype
+char	*get_next_line(int fd);
+size_t	ft_strlen(const char *str);
+char	*ft_strdup(const char *s);
+char	*ft_strchr(const char *src, int c);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+
+#endif
